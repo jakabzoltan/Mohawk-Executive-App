9bd5641e6d4fa12b9a5cae26c47cf8c6770c2aab
Zoltan Jakab <zoltan.jakab1@mohawkcollege.ca>
2018-02-14 14:18:42 -0500

    Added Web Project, Services and Database Project.

---
+++ Mohawk.Executive.App/Mohawk.Executive/Class1.cs
@@ -0,0 +1,12 @@
+﻿Dusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mohawk.Executive
+{
+    public class Class1
+    {
+    }
+}
